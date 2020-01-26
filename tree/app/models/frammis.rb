13066db4c2ab6c1bb7e8ce0091d8d73c8edefe4c
sisisin <azsisisin@gmail.com>
2020-01-26 12:38:31 +0900

    update

--- tree/app/models/frammis.rb
+++ tree/app/models/frammis.rb
@@ -1,3 +1,9 @@
 class Frammis < ApplicationRecord
   self.table_name = 'frammis'
+
+  class << self
+    def calc_weight(part)
+      select(sanitize_sql_array(["WgtCalc2(?)", part]))
+    end
+  end
 end
